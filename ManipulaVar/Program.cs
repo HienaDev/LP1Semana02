8e34dcb9373a4240f96364732f492102a5d33557
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-08 21:17:00 +0000

    Added ++

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ManipulaVar
+{ 
+    class Program
+    {
+        // This program receives an argument and prints the value after and 
+        // before itself
+        static void Main(string[] args)
+        {
+            int x = int.Parse(args[0]);
+
+            x++;
+            
+            Console.WriteLine(x);
+        }
+    }
+}
