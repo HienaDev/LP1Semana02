9b7031fa697b9645c3551745ec674c0a76635042
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-09 15:26:12 +0000

    Added questions

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float pi = 3.1415926f;
+
+            Console.WriteLine("Whats the height of the cylinder? \n ");
+            float height = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("\nWhats the radius of the cylinder? \n ");
+            float radius = Convert.ToSingle(Console.ReadLine());
+
+            
+        }
+    }
+}
