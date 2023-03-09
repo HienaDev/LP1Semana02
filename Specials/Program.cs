e44194abfe40f728f9c1b139a0fb3813e914b5da
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-09 16:26:11 +0000

    Added overflows

--- Specials/Program.cs
+++ Specials/Program.cs
@@ -8,56 +8,67 @@ namespace Specials
         // Also prints positive and negative infinity and Nan
         static void Main(string[] args)
         {
+            Console.WriteLine("Int:");
             Console.WriteLine(int.MinValue);
             Console.WriteLine(int.MaxValue);
 
             Console.WriteLine();
 
+            Console.WriteLine("UInt:");
             Console.WriteLine(uint.MinValue);
             Console.WriteLine(uint.MaxValue);
 
             Console.WriteLine();
             
+            Console.WriteLine("SByte:");
             Console.WriteLine(sbyte.MinValue);
             Console.WriteLine(sbyte.MaxValue);
 
             Console.WriteLine();
 
+            Console.WriteLine("Byte:");
             Console.WriteLine(byte.MinValue);
             Console.WriteLine(byte.MaxValue);
 
             Console.WriteLine();
 
+            Console.WriteLine("Short:");
             Console.WriteLine(short.MinValue);
             Console.WriteLine(short.MaxValue);
 
             Console.WriteLine();
 
+            Console.WriteLine("UShort:");
             Console.WriteLine(ushort.MinValue);
             Console.WriteLine(ushort.MaxValue);
 
             Console.WriteLine();
 
+            Console.WriteLine("Float:");
             Console.WriteLine(float.MinValue);
             Console.WriteLine(float.MaxValue);
 
             Console.WriteLine();
 
+            Console.WriteLine("Decimal:");
             Console.WriteLine(decimal.MinValue);
             Console.WriteLine(decimal.MaxValue);
 
             Console.WriteLine();
 
+            Console.WriteLine("Double:");
             Console.WriteLine(double.MinValue);
             Console.WriteLine(double.MaxValue);
 
             Console.WriteLine();
 
+            Console.WriteLine("Long:");
             Console.WriteLine(long.MinValue);
             Console.WriteLine(long.MaxValue);
 
             Console.WriteLine();
 
+            Console.WriteLine("ULong:");
             Console.WriteLine(ulong.MinValue);
             Console.WriteLine(ulong.MaxValue);
 
@@ -73,6 +84,35 @@ namespace Specials
             Console.WriteLine(float.PositiveInfinity);
             Console.WriteLine(float.NaN);
 
+            Console.WriteLine();
+
+            long maxInt = 2147483647;
+
+            Console.WriteLine((int) maxInt);
+
+            maxInt++;
+              
+            Console.WriteLine((int) maxInt);
+
+            Console.WriteLine();
+
+            double maxFloat = float.MaxValue;
+
+            Console.WriteLine((float) maxFloat);
+
+            maxFloat++;
+              
+            Console.WriteLine((float) maxFloat);
+
+            Console.WriteLine();
+
+            double maxFloat_infinity = float.MaxValue;
+
+            Console.WriteLine((float) maxFloat_infinity);
+
+            maxFloat_infinity *=2;
+              
+            Console.WriteLine((float) maxFloat_infinity);
         }
     }
 }
