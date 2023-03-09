58a54255ab981aeb0066eefa532d611702939a1f
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-09 15:47:58 +0000

    Added max and min values of all types

---
+++ Specials/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        // This program prints max and min values of known types
+        // Also prints positive and negative infinity and Nan
+        static void Main(string[] args)
+        {
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(uint.MaxValue);
+
+            Console.WriteLine();
+            
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(sbyte.MaxValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(byte.MaxValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(short.MaxValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(ushort.MinValue);
+            Console.WriteLine(ushort.MaxValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(ulong.MinValue);
+            Console.WriteLine(ulong.MaxValue);
+
+            
+
+
+            
+        }
+    }
+}
