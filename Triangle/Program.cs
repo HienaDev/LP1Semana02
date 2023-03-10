ac06866a9fdac646cdb7fbbbe538a60c5ce1184b
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-10 13:27:50 +0000

    Created triangle program

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -4,9 +4,24 @@ namespace Triangle
 {
     class Program
     {
+        // This program receives 2 arguments, the size of the triangle
+        // and the character to draw the triangle with
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int size = int.Parse(args[0]);
+            
+            // Goes through each line of the triangle
+            for(int i = 1; i <= size ; i++)
+            {
+                // Prints the selected character every line,
+                // incrementing once each time
+                for(int x = 0; x < i; x++)
+                {
+                    Console.Write(args[1]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
